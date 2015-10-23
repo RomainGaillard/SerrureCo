312ceaa226de9eb9fe3b16d94754ba3dbc08dcf3
valentinhauchecorne@free.fr <valentinhauchecorne@free.fr>
2015-10-23 09:50:16 +0200

    Implémentation controller

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -6,6 +6,33 @@
  */
 
 module.exports = {
-	
+    /**
+     * @description :: Retourne un json avec les logs correspondant à la serrure passé en paramètre
+     */
+    logs: function (req, res) {
+        //Appel LogService::FindByLock(req.param('lock'))
+    },
+    /**
+     * @description :: Retourne un json avec les logs correspondant à la serrure fourni
+     * en paramètre selon un jour choisi
+     */
+    logsByLockAndDate: function (req, res) {
+        /**
+         *Appel LogService::FindByLockAndDate(
+        req.param('lock'),
+         req.param('date')
+         ); */
+    },
+    /**
+     * @description :: Retourne un json avec les logs correspondant à la serrure fourni en paramètre
+     * selon l'interval de date
+     */
+    logsByLockAndDualDate: function (req, res) {
+        //Appel LogService::FindByLockAndDualDate(
+        // req.param('lock'),
+        // req.param('start'),
+        // req.param('end')
+        // );
+    }
 };
 
