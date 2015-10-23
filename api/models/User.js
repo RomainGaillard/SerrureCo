c50ffef36f3d7d83e379289d3093170c5b81467c
Bastien <gbastien2@hotmail.fr>
2015-10-23 16:16:17 +0200

    Authentification semi fonctionnelle on peut créer un compte s'y log mais on ne reste pas log

--- api/models/User.js
+++ api/models/User.js
@@ -1,52 +1,12 @@
-/**
- * User.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/#!documentation/models
- */
+var User = {
+  // Enforce model schema in the case of schemaless databases
+  schema: true,
 
-module.exports = {
-
-    attributes: {
-        email: {
-            type: 'email',
-            required: true,
-            unique: true
-        },
-        password: {
-            type: 'string',
-            required: true
-        },
-        firstname: {
-            type: 'string',
-            defaultsTo: 'Account',
-        },
-        lastname: {
-            type: 'string',
-            defaultsTo: 'Unnamed',
-        },
-        groups:{
-            collection:'GroupUser',
-            via:'group'
-        },
-        toJSON: function() {
-            var obj = this.toObject();
-            delete obj.password;
-            return obj;
-        }
-    },
-
-    beforeCreate: function(user, cb) {
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(user.password, salt, function(err, hash) {
-                if (err) {
-                    console.log(err);
-                    cb(err);
-                }else{
-                    user.password = hash;
-                    cb(null, user);
-                }
-            });
-        });
-    }
+  attributes: {
+    username  : { type: 'string', unique: true },
+    email     : { type: 'email',  unique: true },
+    passports : { collection: 'Passport', via: 'user' }
+  }
 };
+
+module.exports = User;
