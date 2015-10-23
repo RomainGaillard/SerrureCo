0547d5ac167f987723b0835d895fb0a0602e99dd
valentinhauchecorne@free.fr <valentinhauchecorne@free.fr>
2015-10-23 09:23:33 +0200

    Implémentation models Log

---
+++ api/models/Log.js
@@ -0,0 +1,26 @@
+/**
+* Log.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      message: {
+          type: 'string',
+          required: true
+      },
+      lock:{
+          type: 'int',
+          columnName: 'lock_id'
+      },
+      user:{
+          type: 'int',
+          columnName: 'user_id'
+      }
+
+  }
+};
+
