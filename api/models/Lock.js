d1403d4761d3136984be54eaf07859117b28911f
RomainGaillard <romain.gaillard@y-nov.com>
2015-10-23 09:24:38 +0200

    Ajout du models Lock.js

---
+++ api/models/Lock.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Romain Gaillard on 23/10/2015.
+ * Lock: Modèle de la serrure connecté.
+ */
+
+module.exports = {
+    attributes: {
+        name:{
+            type: "string",
+            required: true
+        },
+        addressMac:{
+            type: "string",
+            unique: true
+        },
+        state:{
+            type: "boolean"
+        },
+        hasCamera:{
+            type:"boolean"
+        },
+        hasBell:{
+            type:"boolean"
+        },
+        hasMicro:{
+            type:"boolean"
+        },
+        planning:{
+            type:"json"
+        },
+        isRegister:{
+            type:"boolean"
+        }
+    }
+};
