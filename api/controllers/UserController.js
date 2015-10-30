941a2e310b08cdb906ffc7ff557db72394ded407
Bastien <gbastien2@hotmail.fr>
2015-10-30 17:14:56 +0100

    Ajout de l'update du user on peut maintenant modifier ses données firstname lastname et email

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -12,11 +12,43 @@ module.exports = {
         var name = req.param('firstname');
         var lastname = req.param('lastname');
         var email = req.param('email');
-        console.log(req.passport.user.username)
-        User = req.passport.user;
-        User.update({firstname: name}, {lastname: lastname}, {email: email}).exec(function(err, callback) {
-            console.log(err)
+        console.log(req.passport.user)
+        User.findOne({id: req.passport.user.id}).exec(function (err, user) {
+            if(err) {
+                console.log(err)
+            }
+
+            user.email = email;
+            user.firstname = name;
+            user.lastname = lastname;
+
+            user.save(function(err) {
+                if(err) {
+                    console.log("save----" + err)
+                    return res.send(err.status, err);
+                }
+                return res.send(200, user);
+            })
+
         })
+        //User.update({firstname: req.passport.user.firstname},{firstname: name}, {lastname: req.passport.user.lastname}, {lastname: lastname}, {email: req.passport.user.email}, {email: email}).exec(function(err, callback) {
+        //    if(err){
+        //        console.log(err);
+        //        res.badRequest();
+        //    }
+        //    res.ok()
+        //
+        //})
+
+        //User.update({username: req.passport.user.username},{firstname: req.passport.user.firstname}, {firstname: name}, {lastname: req.passport.user.lastname}, {lastname: lastname}, {email: req.passport.user.email}, {email: email}).exec(function(err, callback) {
+        //    if(err){
+        //        console.log(err);
+        //        res.badRequest();
+        //    }
+        //    res.ok()
+        //
+        //})
+
         //req.passport.user.update({firstname: name}, {lastname: lastname}, {email: email}).exec(function(err, callback) {
         //    console.log(err)
         //})
