25ae3ec2af88ed5125f1bdf3b3efb3b20b60e02f
93scorpion76 <valentinhauchecorne@free.fr>
2015-11-22 18:09:45 +0100

    LogService : Populate User

--- api/services/LogService.js
+++ api/services/LogService.js
@@ -1,11 +1,13 @@
 // File location: /api/services/LogService.js
-
+//
+//var log = require('../models/Log.js');
+//var user = require('../models/User.js');
 var LogService = {
     /**
      * @description :: Récupère dans la bdd les logs correspondant à la serrure fourni en paramètre
      */
     findByLock: function findByLockService(lock, callback ) {
-        Log.find({ where: { lock: lock }, limit: 200 }).exec(function (err, found){
+        Log.find({ where: { lock: lock }, limit: 200 }).populate("user").exec(function (err, found){
             if (found) {
                 callback(null,found);
             } else {
@@ -17,7 +19,7 @@ var LogService = {
      * @description :: Récupère dans la bdd les logs correspondant à la serrure et la date fournis en paramètre
      */
     findByLockAndDate: function findByLockAndDateService(lock, date, callback) {
-        Log.find({ where: { lock: lock, createdAt: {'contains' : date} } }).exec(function (err, found){
+        Log.find({ where: { lock: lock, createdAt: {'contains' : date} } }).populate("user").exec(function (err, found){
             if (found) {
                 callback(null,found);
             } else {
@@ -30,9 +32,31 @@ var LogService = {
      * de date fournis en paramètre
      */
     findByLockAndDualDate: function findByLockAndDualDateService(lock, start, end, callback) {
-        Log.query('SELECT * FROM Log where log.createdAt >= \"'+start+'\" AND log.createdAt <= \"'+end+'\"', function (err, found){
+        var request = 'SELECT `l`.`message`, `l`.`id`, `l`.`createdAt`, `l`.`updatedAt`, `l`.`lock_id`, `l`.`user_id`, `u`.`username` ' +
+            'FROM `log` `l` ' +
+            'INNER JOIN `user` `u` ON `l`.`user_id`=`u`.`id` ' +
+            'WHERE `l`.`createdAt` >= \"'+start+'\" AND `l`.`createdAt` <= \"'+end+'\"';
+        Log.query(request, function (err, found){
             if (found) {
-                callback(null,found);
+                var logs    = Array();
+                //var user    = Array();
+                //var log     = Array();
+
+                for(var i =0; i < found.length; i++) {
+                    var user = {
+                        username:     found[i].username
+                    };
+                    var log = {
+                        message:     found[i].message,
+                        createdAt:   found[i].createdAt,
+                        updatedAt:   found[i].updatedAt,
+                        lock:        found[i].lock_id,
+                        user:        user
+                    };
+                    
+                    logs[i]            = log;
+                }
+                callback(null,logs);
             } else {
                 callback(err, null);
             }
